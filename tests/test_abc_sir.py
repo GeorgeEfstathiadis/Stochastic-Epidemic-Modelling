d77e78cd490e81129f85a8412023ec5caa392b97
GeorgeEfstathiadis <gioefst2000@gmail.com>
2021-12-13 12:04:03 +0000

    plots for abc sir

--- tests/test_abc_sir.py
+++ tests/test_abc_sir.py
@@ -21,17 +21,19 @@ t = np.linspace(0, 14, num=200)
 y0 = (4800, 20, 0)
 beta, gamma = 2, 1
 
-dataset = sir_simulate_discrete(y0, t, beta, gamma)
+# dataset = sir_simulate_discrete(y0, t, beta, gamma)
 
-# noisy processs
-data1 = np.array([[0, 0, 0]])
-ratio_noise = 0.1
-for t in range(dataset.shape[0]):
-    sus = dataset.iloc[t, 1] + np.random.normal(0, ratio_noise * dataset.iloc[t, 1])
-    inf = dataset.iloc[t, 2] + np.random.normal(0, ratio_noise * dataset.iloc[t, 2])
-    rem = dataset.iloc[t, 3] + np.random.normal(0, ratio_noise * dataset.iloc[t, 3])
-    data1 = np.append(data1, [[sus, inf, rem]], axis=0)
-data1 = data1[1:]
+# # noisy processs
+# data1 = np.array([[0, 0, 0]])
+# ratio_noise = 0.1
+# for t in range(dataset.shape[0]):
+#     sus = dataset.iloc[t, 1] + np.random.normal(0, ratio_noise * dataset.iloc[t, 1])
+#     inf = dataset.iloc[t, 2] + np.random.normal(0, ratio_noise * dataset.iloc[t, 2])
+#     rem = dataset.iloc[t, 3] + np.random.normal(0, ratio_noise * dataset.iloc[t, 3])
+#     data1 = np.append(data1, [[sus, inf, rem]], axis=0)
+# data1 = data1[1:]
+
+data1 = np.loadtxt("data/simulated_datasets/sir_noise.csv", delimiter=", ")
 
 # plt.plot(range(15), dataset.iloc[:, 1], label='Susceptible')
 # plt.plot(range(15), dataset.iloc[:, 2], label='Infected')
@@ -69,56 +71,56 @@ np.savetxt(results_directory + "sampled_trajs_recovered.csv", sampled_trajs[:, :
 # sampled_trajs_recovered = np.loadtxt(results_directory + "sampled_trajs_recovered.csv", delimiter=",")
 # sampled_trajs = np.stack((sampled_trajs_susceptible, sampled_trajs_infected, sampled_trajs_recovered), axis=-1)
 
-## plot thetas
-plt.plot(
-    range(thetas.shape[0]),
-    thetas[:, 0],
-    color="blue",
-    linewidth=1,
-    label="beta",
-)
-plt.plot(
-    range(thetas.shape[0]),
-    thetas[:, 1],
-    color="orange",
-    linewidth=1,
-    label="gamma",
-)
-plt.legend()
-plt.savefig(graphs_directory + "beta_gamma.png", bbox_inches='tight')
-plt.show()
+# ## plot thetas
+# plt.plot(
+#     range(thetas.shape[0]),
+#     thetas[:, 0],
+#     color="blue",
+#     linewidth=1,
+#     label="beta",
+# )
+# plt.plot(
+#     range(thetas.shape[0]),
+#     thetas[:, 1],
+#     color="orange",
+#     linewidth=1,
+#     label="gamma",
+# )
+# plt.legend()
+# plt.savefig(graphs_directory + "beta_gamma.png", bbox_inches='tight')
+# plt.show()
 
-## plot thetas - scatterplot
-plt.scatter(thetas[:, 0], thetas[:, 1])
-plt.scatter(beta, gamma, color="red")
-plt.savefig(graphs_directory + "beta_gamma2.png", bbox_inches='tight')
-plt.show()
+# ## plot thetas - scatterplot
+# plt.scatter(thetas[:, 0], thetas[:, 1])
+# plt.scatter(beta, gamma, color="red")
+# plt.savefig(graphs_directory + "beta_gamma2.png", bbox_inches='tight')
+# plt.show()
 
-## plot thetas - histogram
-plt.hist(thetas[:, 0], bins=10, alpha=0.5, label="beta")
-plt.hist(thetas[:, 1], bins=10, alpha=0.5, label="gamma")
-plt.axvline(beta)
-plt.axvline(gamma)
-plt.legend()
-plt.savefig(graphs_directory + "beta_gamma3.png", bbox_inches='tight')
-plt.show()
+# ## plot thetas - histogram
+# plt.hist(thetas[:, 0], bins=10, alpha=0.5, label="beta")
+# plt.hist(thetas[:, 1], bins=10, alpha=0.5, label="gamma")
+# plt.axvline(beta)
+# plt.axvline(gamma)
+# plt.legend()
+# plt.savefig(graphs_directory + "beta_gamma3.png", bbox_inches='tight')
+# plt.show()
 
 
-## plot trajectories
-lines1 = plt.plot(
-    range(len(dataset)), sampled_trajs[:, :, 1].transpose(), "orange", linewidth=1
-)
-plt.plot(
-    range(len(dataset)), sampled_trajs[:, :, 2].transpose(), "orange", linewidth=1
-)
-plt.plot(
-    range(len(dataset)), sampled_trajs[:, :, 3].transpose(), "orange", linewidth=1
-)
-lines4 = plt.plot(range(len(dataset)), dataset.iloc[:, 1:], "k", linewidth=2)
-lines5 = plt.plot(range(len(dataset)), data1, "k--", linewidth=2)
-plt.legend(
-    lines1[:1] + lines4 + lines5,
-    ["simulated trajectories", "hidden compartments", "observed compartments"],
-)
-plt.savefig(graphs_directory + "trajectories.png", bbox_inches='tight')
-plt.show()
+# ## plot trajectories
+# lines1 = plt.plot(
+#     range(len(dataset)), sampled_trajs[:, :, 1].transpose(), "orange", linewidth=1
+# )
+# plt.plot(
+#     range(len(dataset)), sampled_trajs[:, :, 2].transpose(), "orange", linewidth=1
+# )
+# plt.plot(
+#     range(len(dataset)), sampled_trajs[:, :, 3].transpose(), "orange", linewidth=1
+# )
+# lines4 = plt.plot(range(len(dataset)), dataset.iloc[:, 1:], "k", linewidth=2)
+# lines5 = plt.plot(range(len(dataset)), data1, "k--", linewidth=2)
+# plt.legend(
+#     lines1[:1] + lines4 + lines5,
+#     ["simulated trajectories", "hidden compartments", "observed compartments"],
+# )
+# plt.savefig(graphs_directory + "trajectories.png", bbox_inches='tight')
+# plt.show()
