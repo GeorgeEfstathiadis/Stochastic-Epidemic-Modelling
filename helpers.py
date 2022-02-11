add running average fn and fix bugs in other fns

helpers.py
@@ -4,7 +4,12 @@ import scipy.stats
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
     n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
+    if len(a.shape) == 2:
+        m = np.mean(a, axis=1)
+        se = scipy.stats.sem(a, axis=1)
+    else:
+        m = np.mean(a)
+        se = scipy.stats.sem(a)
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
     return m, m-h, m+h
 
@@ -30,7 +35,7 @@ def gelman_rubin_test(chains):
     W = np.mean(var_params, axis=0)
 
     # compute how indiv. means scatter around the joint mean
-    B = N / (M - 1) * np.sum((mean_params - theta_hat) ** 2)
+    B = N / (M - 1) * np.sum((mean_params - theta_hat) ** 2, axis=0)
 
     # compute the estimated variance of the joint mean
     V = (N - 1) / N * W + (M + 1) / (M * N) * B
@@ -39,5 +44,7 @@ def gelman_rubin_test(chains):
     return np.sqrt(V / W)
 
 
-
-    
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+ 